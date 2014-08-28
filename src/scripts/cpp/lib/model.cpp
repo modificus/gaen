7004ce7c8ac82e753cc3517c7fdcbd1335bd0954
Lachlan Orr <lorr@gaen.org>
2014-08-28 08:16:35 -0500

    Support for import statement in Compose scripts
    - Added import statement parsing
    - Also now allow use of identifiers or types, which will lead to support
      for structs
    - Started work on support for function libraries in Compose
    - Compiler can now generate header files info to, which will be needed
      for the function library support when generating cpp

---
+++ src/scripts/cpp/lib/model.cpp
@@ -0,0 +1,38 @@
+//------------------------------------------------------------------------------
+// model.cpp - Auto-generated from model.cmp
+//
+// This software is provided 'as-is', without any express or implied
+// warranty. In no event will the authors be held liable for any damages
+// arising from the use of this software.
+// 
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+// 
+//   1. The origin of this software must not be misrepresented; you must not
+//   claim that you wrote the original software. If you use this software
+//   in a product, an acknowledgment in the product documentation would be
+//   appreciated but is not required.
+// 
+//   2. Altered source versions must be plainly marked as such, and must not be
+//   misrepresented as being the original software.
+// 
+//   3. This notice may not be removed or altered from any source
+//   distribution.
+//------------------------------------------------------------------------------
+
+// HASH: c7c8a2221b0167f04bc48f2ea1eb7dd9
+#include "engine/hashes.h"
+#include "engine/Block.h"
+#include "engine/MessageWriter.h"
+#include "engine/Task.h"
+#include "engine/Registry.h"
+#include "engine/Component.h"
+#include "engine/Entity.h"
+
+namespace gaen
+{
+
+
+} // namespace gaen
+
