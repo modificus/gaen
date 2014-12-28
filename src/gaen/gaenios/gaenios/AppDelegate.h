1040ec64ac3ee4693dac2a454cfa3a99f8d17c0a
Lachlan Orr <lorr@gaen.org>
2014-12-28 14:40:48 -0600

    Initial commit of a simple xcode project. Committing now before I start mucking about with it.

---
+++ src/gaen/gaenios/gaenios/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gaenios
+//
+//  Created by Lachlan Orr on 12/28/14.
+//  Copyright (c) 2014 Lachlan Orr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ src/gaen/gaenios/gaenios/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gaenios
+//
+//  Created by Lachlan Orr on 12/28/14.
+//  Copyright (c) 2014 Lachlan Orr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
