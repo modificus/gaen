6d024d3773d5ff35c6c37d60f1d52b4a7d2c0c41
Lachlan Orr <lorr@gaen.org>
2015-02-11 03:53:35 -0600

    Added error checking to project specific bootstrap.bat

--- python/templates/project/bootstrap.bat
+++ python/templates/project/bootstrap.bat
@@ -6,11 +6,16 @@ SETLOCAL
 :: Change to root dir
 CD /d %~dp0
 
+:: Make sure we're in a clean state
+call .\clean.bat
+if %errorlevel% neq 0 exit /b %errorlevel%
+
 :: Clone gaen if it doesn't exist. This will need to happen for
 :: anyone who has cloned the project specific repository and is running
 :: bootstrap.bat for the first time.
 IF NOT EXIST .\gaen (
   git clone <<GAEN_GIT_REMOTE>> gaen
+  if %errorlevel% neq 0 exit /b %errorlevel%
 )
 
 :: Write root directory to main <<PROJECT_NAME_UPPER>>_ROOT env var
@@ -34,38 +39,47 @@ set BUILD_DIR=%<<PROJECT_NAME_UPPER>>_ROOT%\build\%PLAT%
 
 if not exist "%BUILD_DIR%" (
    mkdir "%BUILD_DIR%"
+   if %errorlevel% neq 0 exit /b %errorlevel%
 )
 
 
 :: Create our project specific system_api_meta.cpp
 python "%~dp0\gaen\python\codegen_api.py"
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 :: Issue cmake command
 cd %BUILD_DIR%
 if "%PLAT%"=="win64" (
     cmake -G "Visual Studio 12 Win64" %<<PROJECT_NAME_UPPER>>_ROOT%
+    if %errorlevel% neq 0 exit /b %errorlevel%
 )
 if "%PLAT%"=="win32" (
     cmake -G "Visual Studio 12" %<<PROJECT_NAME_UPPER>>_ROOT%
+    if %errorlevel% neq 0 exit /b %errorlevel%
 )
 
 :: Build cmpc so we can run codegen.py and boostrap our scripts
 call "%VS120COMNTOOLS%\vsvars32.bat"
+if %errorlevel% neq 0 exit /b %errorlevel%
 msbuild "%BUILD_DIR%\gaen\src\cmpc\cmpc.vcxproj"
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 :: Do the .cmp -> .cpp codegen
 echo.
 echo Running cmpc to compile Compose scripts...
 python "%~dp0\gaen\python\codegen.py"
+if %errorlevel% neq 0 exit /b %errorlevel%
 echo.
 
 :: Re-run cmake since the compiled scripts have been added to
 :: the cmake files.
 if "%PLAT%"=="win64" (
     cmake -G "Visual Studio 12 Win64" %<<PROJECT_NAME_UPPER>>_ROOT%
+    if %errorlevel% neq 0 exit /b %errorlevel%
 )
 if "%PLAT%"=="win32" (
     cmake -G "Visual Studio 12" %<<PROJECT_NAME_UPPER>>_ROOT%
+    if %errorlevel% neq 0 exit /b %errorlevel%
 )
 
 echo.
