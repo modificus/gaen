bce43e0034336c99103a092caafa5f3eb5b5216a
Lachlan Orr <lorr@gaen.org>
2016-05-15 08:38:59 -0500

    WIP - Asset management within the Engine
    - Added some files to manage assets at runtime
    - Will manage file access, loading, unloading, etc.

---
+++ src/engine/Asset.cpp
@@ -0,0 +1,63 @@
+//------------------------------------------------------------------------------
+// Asset.cpp - Smart wrapper for raw asset buffers
+//
+// Gaen Concurrency Engine - http://gaen.org
+// Copyright (c) 2014-2016 Lachlan Orr
+//
+// This software is provided 'as-is', without any express or implied
+// warranty. In no event will the authors be held liable for any damages
+// arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+//
+//   1. The origin of this software must not be misrepresented; you must not
+//   claim that you wrote the original software. If you use this software
+//   in a product, an acknowledgment in the product documentation would be
+//   appreciated but is not required.
+//
+//   2. Altered source versions must be plainly marked as such, and must not be
+//   misrepresented as being the original software.
+//
+//   3. This notice may not be removed or altered from any source
+//   distribution.
+//------------------------------------------------------------------------------
+
+#include "engine/stdafx.h"
+
+#include "engine/Asset.h"
+
+namespace gaen
+{
+
+Asset::Asset(const char * path)
+  : refCount(0)
+{
+    ASSERT(path);
+
+    const u8 * pBuffer = load(path);
+
+    if (pBuffer != nullptr)
+    {
+        pParts = GNEW(kMEM_Engine, Asset::AssetParts);
+
+        u32 pathLen = strlen(path);
+        pParts->path = GALLOC(kMEM_Engine, pathLen+1);
+        strncpy(pParts->path, path, pathLen);
+        pParts->path[pathLen] = '\0';
+
+        pParts->pBuffer = pBuffer;
+        pParts->refCount++;
+
+        ASSERT((pParts && pParts->path && pParts->pBuffer && refCount > 0) ||
+               !pParts);
+    }
+}
+
+const u8 * Asset::load(const char * path)
+{
+    
+}
+
+} // namespace gaen
---
+++ src/engine/AssetMgr.cpp
@@ -0,0 +1,99 @@
+//------------------------------------------------------------------------------
+// AssetMgr.cpp - Loads and unloads raw asset buffers
+//
+// Gaen Concurrency Engine - http://gaen.org
+// Copyright (c) 2014-2016 Lachlan Orr
+//
+// This software is provided 'as-is', without any express or implied
+// warranty. In no event will the authors be held liable for any damages
+// arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+//
+//   1. The origin of this software must not be misrepresented; you must not
+//   claim that you wrote the original software. If you use this software
+//   in a product, an acknowledgment in the product documentation would be
+//   appreciated but is not required.
+//
+//   2. Altered source versions must be plainly marked as such, and must not be
+//   misrepresented as being the original software.
+//
+//   3. This notice may not be removed or altered from any source
+//   distribution.
+//------------------------------------------------------------------------------
+
+#include "engine/stdafx.h"
+
+#include "engine/AssetMgr.h"
+
+namespace gaen
+{
+
+template <typename T>
+MessageResult AssetMgr::message(const T & msgAcc)
+{
+    const Message & msg = msgAcc.message();
+
+	/*
+	switch(msg.msgId)
+    {
+
+    case HASH::renderer_insert_model_instance:
+    {
+        messages::InsertModelInstanceR<T> msgr(msgAcc);
+        mpModelMgr->insertModelInstance(msgAcc.message().source,
+                                        msgr.uid(),
+                                        msgr.model(),
+                                        msgr.transform(),
+                                        msgr.isAssetManaged());
+        break;
+    }
+    case HASH::renderer_transform_model_instance:
+    {
+        messages::TransformIdR<T> msgr(msgAcc);
+        ModelInstance * pModelInst = mpModelMgr->findModelInstance(msgr.id());
+        ASSERT(pModelInst);
+        pModelInst->transform = msgr.transform();
+        break;
+    }
+    case HASH::renderer_remove_model_instance:
+    {
+        mpModelMgr->removeModelInstance(msg.source, msg.payload.u);
+        break;
+    }
+    case HASH::renderer_insert_light_directional:
+    {
+        messages::InsertLightDirectionalR<T> msgr(msgAcc);
+        glm::vec3 normDir = glm::normalize(msgr.direction());
+        glm::vec3 relDir = -normDir; // flip direction of vector relative to objects
+        mDirectionalLights.emplace_back(msgAcc.message().source,
+                                        msgr.uid(),
+                                        relDir,
+                                        msgr.color());
+        break;
+    }
+    case HASH::renderer_update_light_directional:
+    {
+        messages::InsertLightDirectionalR<T> msgr(msgAcc);
+        mDirectionalLights.emplace_back(msgAcc.message().source,
+                                        msgr.uid(),
+                                        msgr.direction(),
+                                        msgr.color());
+        break;
+    }
+    case HASH::renderer_move_camera:
+    {
+        messages::MoveCameraR<T> msgr(msgAcc);
+        mRaycastCamera.move(msgr.position(), msgr.direction());
+        break;
+    }
+    default:
+        PANIC("Unknown renderer message: %d", msg.msgId);
+    }
+*/
+    return MessageResult::Consumed;
+}
+
+} // namespace gaen
